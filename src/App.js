463339807253b2528438d92f8d2f4fdf21acae69
sanya-shch <shchipkov.sanya@ukr.net>
2022-12-10 17:17:38 +0200

    clean cra and initialize mobx

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { Routes, Route } from "react-router-dom";
+
+import HomePage from "../src/screens/Home";
+import GamePage from "../src/screens/Game";
 
 function App() {
+  const [gameId, setGameId] = useState("");
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route
+        index
+        element={<HomePage gameId={gameId} setGameId={setGameId} />}
+      />
+      <Route path="/game/:id" element={<GamePage />} />
+    </Routes>
   );
 }
 
---
+++ src/screens/Home/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const HomePage = () => {
+
+  return (
+    <div>home</div>
+  )
+};
+
+export default HomePage;
